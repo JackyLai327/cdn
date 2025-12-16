8824035732dab7294ffc1debcff50cd3803e6517
Jacky Lai (Work) <jackylai327@gmail.com>
2025-12-16 16:58:36 +1100

    style: align variable assignments and tags in RDS configurations

--- infra/envs/prod/rds.tf
+++ infra/envs/prod/rds.tf
@@ -2,24 +2,24 @@ module "rds" {
   source = "../../modules/rds"
 
   # * Basic identifiers
-  db_name = "cdn_db"
+  db_name     = "cdn_db"
   db_username = "cdn"
 
   # * Engine details
-  db_engine = "postgres"
+  db_engine      = "postgres"
   engine_version = "15"
   instance_class = "db.t3.micro"
 
   # * Networking (Connecting to existing VPC)
-  vpc_id = module.vpc.vpc_id
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnet_ids
 
   # * Security (Connecting to existing EKS module)
   eks_node_sg_id = module.eks.node_security_group_id
 
   tags = {
-    Name = "cdn_db"
+    Name        = "cdn_db"
     Environment = "prod"
-    Project = "cdn"
+    Project     = "cdn"
   }
 }
